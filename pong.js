2d65dff31b3196aa338e0f809627b9bf9062fb6a
thibaultbain <45803361+thibaultbain@users.noreply.github.com>
2023-03-15 10:42:09 +1100

    Update pong.js

--- pong.js
+++ pong.js
@@ -1,93 +1,75 @@
-const game = new Phaser.Game(640, 480, Phaser.AUTO, 'gameDiv', { preload: preload, create: create, update: update });
+// create a new Phaser game instance with a canvas of size 800x600px
+const game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
+  preload: preload,
+  create: create,
+  update: update
+});
 
+// declare variables
+let paddleLeft;
+let paddleRight;
 let ball;
-let ballVelocityX = 150;
-let ballVelocityY = -150;
+let ballVelocity = 200;
+let ballLastHit = '';
 
-let playerPaddle;
-let computerPaddle;
-let paddleSpeed = 400;
-
-let playerScore = 0;
-let computerScore = 0;
-let scoreText;
-
+// preload function - called first
 function preload() {
-  // Nothing to preload since we're creating the ball and paddles programmatically
+  // load paddle image
+  game.load.image('paddle', 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAFklEQVR4nGJiYGAABAAA//8Af4V4jkAAAAASUVORK5CYII=');
+  // load ball image
+  game.load.image('ball', 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAFklEQVR4nGJiYGAABAAA//8Af4V4jkAAAAASUVORK5CYII=');
 }
 
+// create function - called once after preload is finished
 function create() {
-  createBall();
-  createPaddles();
-  createScoreText();
+  // add paddles to the game
+  paddleLeft = game.add.sprite(20, game.world.centerY, 'paddle');
+  paddleLeft.anchor.setTo(0.5, 0.5);
+  paddleRight = game.add.sprite(game.world.width - 20, game.world.centerY, 'paddle');
+  paddleRight.anchor.setTo(0.5, 0.5);
+
+  // add ball to the game
+  ball = game.add.sprite(game.world.centerX, game.world.centerY, 'ball');
+  ball.anchor.setTo(0.5, 0.5);
+
+  // enable physics for the paddles and ball
+  game.physics.enable([paddleLeft, paddleRight, ball], Phaser.Physics.ARCADE);
+
+  // make paddles immovable
+  paddleLeft.body.immovable = true;
+  paddleRight.body.immovable = true;
+
+  // set ball velocity
+  ball.body.velocity.setTo(ballVelocity, ballVelocity);
+  ball.body.bounce.setTo(1, 1);
+
+  // add collision detection for the ball and paddles
+  game.physics.arcade.collide(paddleLeft, ball, function () {
+    ball.body.velocity.x = ballVelocity;
+    ballLastHit = 'left';
+  });
+  game.physics.arcade.collide(paddleRight, ball, function () {
+    ball.body.velocity.x = -ballVelocity;
+    ballLastHit = 'right';
+  });
+
+  // add event listener for arrow key input
+  game.input.keyboard.addKeyCapture([Phaser.Keyboard.UP, Phaser.Keyboard.DOWN, Phaser.Keyboard.W, Phaser.Keyboard.S]);
 }
 
+// update function - called every frame
 function update() {
-  movePlayerPaddle();
-  moveComputerPaddle();
-  checkBallPaddleCollision(playerPaddle);
-  checkBallPaddleCollision(computerPaddle);
-  checkBallWallCollision();
-  updateScore();
-}
-
-function createBall() {
-  const ballRadius = 10;
-
-  ball = game.add.graphics();
-  ball.beginFill(0xffffff);
-  ball.drawCircle(0, 0, ballRadius);
-  ball.x = game.world.centerX;
-  ball.y = game.world.centerY;
-}
-
-function createPaddles() {
-  const paddleWidth = 20;
-  const paddleHeight = 80;
-  const paddleDistanceFromEdge = 50;
-
-  // Player paddle
-  playerPaddle = game.add.graphics();
-  playerPaddle.beginFill(0xffffff);
-  playerPaddle.drawRect(0, 0, paddleWidth, paddleHeight);
-  playerPaddle.x = paddleDistanceFromEdge;
-  playerPaddle.y = game.world.centerY - paddleHeight / 2;
-
-  // Computer paddle
-  computerPaddle = game.add.graphics();
-  computerPaddle.beginFill(0xffffff);
-  computerPaddle.drawRect(0, 0, paddleWidth, paddleHeight);
-  computerPaddle.x = game.world.width - paddleWidth - paddleDistanceFromEdge;
-  computerPaddle.y = game.world.centerY - paddleHeight / 2;
-}
-
-function createScoreText() {
-  const textStyle = { font: "32px Arial", fill: "#ffffff", align: "center" };
-  scoreText = game.add.text(game.world.centerX, 50, `${playerScore} - ${computerScore}`, textStyle);
-  scoreText.anchor.setTo(0.5, 0);
-}
-
-function movePlayerPaddle() {
-  if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
-    playerPaddle.y -= paddleSpeed * game.time.physicsElapsed;
-  } else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
-    playerPaddle.y += paddleSpeed * game.time.physicsElapsed;
+  // move paddles with arrow key input
+  if (game.input.keyboard.isDown(Phaser.Keyboard.UP) || game.input.keyboard.isDown(Phaser.Keyboard.W)) {
+    paddleLeft.body.velocity.y = -300;
+  } else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN) || game.input.keyboard.isDown(Phaser.Keyboard.S)) {
+    paddleLeft.body.velocity.y = 300;
+  } else {
+    paddleLeft.body.velocity.y = 0;
   }
-}
 
-function moveComputerPaddle() {
-  const computerPaddleSpeed = 200;
-
-  if (ball.y < computerPaddle.y + computerPaddle.height / 2) {
-    computerPaddle.y -= computerPaddleSpeed * game.time.physicsElapsed;
-  } else if (ball.y > computerPaddle.y + computerPaddle.height / 2) {
-    computerPaddle.y += computerPaddleSpeed * game.time.physicsElapsed;
-  }
-}
-
-function checkBallPaddleCollision(paddle) {
-  const collisionPadding = 5;
-
-  if (ball.x + ball.width / 2 >= paddle.x && ball.x - ball.width / 2 <= paddle.x + paddle.width) {
-    if (ball.y + ball.height / 2 + collisionPadding >= paddle.y && ball.y - ball.height / 2 - collisionPadding <= paddle.y + paddle.height) {
-      ballVelocityX = -ballVelocityX;
+  // move computer-controlled paddle
+  if (ball.y > paddleRight.y) {
+    paddleRight.body.velocity.y = 300;
+  } else if (ball.y < paddleRight.y) {
+    paddleRight.body
