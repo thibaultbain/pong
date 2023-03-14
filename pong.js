bccc624eb0f8959731eab209cb2aa8597b98121f
thibaultbain <45803361+thibaultbain@users.noreply.github.com>
2023-03-15 10:44:46 +1100

    Update pong.js

--- pong.js
+++ pong.js
@@ -1,75 +1,74 @@
-// create a new Phaser game instance with a canvas of size 800x600px
-const game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
-  preload: preload,
-  create: create,
-  update: update
-});
-
-// declare variables
-let paddleLeft;
-let paddleRight;
-let ball;
-let ballVelocity = 200;
-let ballLastHit = '';
-
-// preload function - called first
-function preload() {
-  // load paddle image
-  game.load.image('paddle', 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAFklEQVR4nGJiYGAABAAA//8Af4V4jkAAAAASUVORK5CYII=');
-  // load ball image
-  game.load.image('ball', 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAFklEQVR4nGJiYGAABAAA//8Af4V4jkAAAAASUVORK5CYII=');
-}
-
-// create function - called once after preload is finished
-function create() {
-  // add paddles to the game
-  paddleLeft = game.add.sprite(20, game.world.centerY, 'paddle');
-  paddleLeft.anchor.setTo(0.5, 0.5);
-  paddleRight = game.add.sprite(game.world.width - 20, game.world.centerY, 'paddle');
-  paddleRight.anchor.setTo(0.5, 0.5);
-
-  // add ball to the game
-  ball = game.add.sprite(game.world.centerX, game.world.centerY, 'ball');
-  ball.anchor.setTo(0.5, 0.5);
-
-  // enable physics for the paddles and ball
-  game.physics.enable([paddleLeft, paddleRight, ball], Phaser.Physics.ARCADE);
-
-  // make paddles immovable
-  paddleLeft.body.immovable = true;
-  paddleRight.body.immovable = true;
-
-  // set ball velocity
-  ball.body.velocity.setTo(ballVelocity, ballVelocity);
-  ball.body.bounce.setTo(1, 1);
-
-  // add collision detection for the ball and paddles
-  game.physics.arcade.collide(paddleLeft, ball, function () {
-    ball.body.velocity.x = ballVelocity;
-    ballLastHit = 'left';
-  });
-  game.physics.arcade.collide(paddleRight, ball, function () {
-    ball.body.velocity.x = -ballVelocity;
-    ballLastHit = 'right';
-  });
-
-  // add event listener for arrow key input
-  game.input.keyboard.addKeyCapture([Phaser.Keyboard.UP, Phaser.Keyboard.DOWN, Phaser.Keyboard.W, Phaser.Keyboard.S]);
-}
-
-// update function - called every frame
-function update() {
-  // move paddles with arrow key input
-  if (game.input.keyboard.isDown(Phaser.Keyboard.UP) || game.input.keyboard.isDown(Phaser.Keyboard.W)) {
-    paddleLeft.body.velocity.y = -300;
-  } else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN) || game.input.keyboard.isDown(Phaser.Keyboard.S)) {
-    paddleLeft.body.velocity.y = 300;
-  } else {
-    paddleLeft.body.velocity.y = 0;
+<script>
+  const gameDiv = document.querySelector('.game');
+  const ball = gameDiv.querySelector('.ball');
+  const paddleA = gameDiv.querySelector('.paddle-a');
+  const paddleB = gameDiv.querySelector('.paddle-b');
+  
+  const ballSpeed = 5;
+  let ballX = 50;
+  let ballY = 50;
+  let ballDirectionX = 1;
+  let ballDirectionY = 1;
+  
+  function updateBall() {
+    ballX += ballSpeed * ballDirectionX;
+    ballY += ballSpeed * ballDirectionY;
+    
+    // Bounce the ball off the walls
+    if (ballX < 0 || ballX > gameDiv.clientWidth - ball.clientWidth) {
+      ballDirectionX *= -1;
+    }
+    if (ballY < 0 || ballY > gameDiv.clientHeight - ball.clientHeight) {
+      ballDirectionY *= -1;
+    }
+    
+    ball.style.transform = `translate(${ballX}px, ${ballY}px)`;
   }
-
-  // move computer-controlled paddle
-  if (ball.y > paddleRight.y) {
-    paddleRight.body.velocity.y = 300;
-  } else if (ball.y < paddleRight.y) {
-    paddleRight.body
+  
+  const paddleSpeed = 5;
+  const paddleHeight = paddleA.clientHeight;
+  let paddleADirection = 0;
+  let paddleBDirection = 0;
+  
+  function updatePaddles() {
+    // Update paddle A position
+    let newPaddleAY = paddleA.getBoundingClientRect().y + (paddleSpeed * paddleADirection);
+    if (newPaddleAY >= 0 && newPaddleAY <= gameDiv.clientHeight - paddleHeight) {
+      paddleA.style.transform = `translateY(${newPaddleAY}px)`;
+    }
+    
+    // Update paddle B position
+    let newPaddleBY = paddleB.getBoundingClientRect().y + (paddleSpeed * paddleBDirection);
+    if (newPaddleBY >= 0 && newPaddleBY <= gameDiv.clientHeight - paddleHeight) {
+      paddleB.style.transform = `translateY(${newPaddleBY}px)`;
+    }
+  }
+  
+  function update() {
+    updateBall();
+    updatePaddles();
+    window.requestAnimationFrame(update);
+  }
+  
+  update();
+  
+  // Handle user input
+  document.addEventListener('keydown', (event) => {
+    if (event.key === 'ArrowUp') {
+      paddleADirection = -1;
+    } else if (event.key === 'ArrowDown') {
+      paddleADirection = 1;
+    } else if (event.key === 'w') {
+      paddleBDirection = -1;
+    } else if (event.key === 's') {
+      paddleBDirection = 1;
+    }
+  });
+  
+  document.addEventListener('keyup', (event) => {
+    if (event.key === 'ArrowUp' || event.key === 'ArrowDown') {
+      paddleADirection = 0;
+    } else if (event.key === 'w' || event.key === 's') {
+      paddleBDirection = 0;
+    }
+  });
