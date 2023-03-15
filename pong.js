9b9583c2f608be87a5884d75b85fafe6cac0f18b
thibaultbain <45803361+thibaultbain@users.noreply.github.com>
2023-03-15 15:01:30 +1100

    Update pong.js

--- pong.js
+++ pong.js
@@ -1,8 +1,8 @@
 const config = {
   type: Phaser.AUTO,
+  width: 800,
+  height: 600,
   parent: 'game-container',
-  width: 600,
-  height: 400,
   physics: {
     default: 'arcade',
     arcade: {
@@ -19,82 +19,112 @@ const config = {
 
 const game = new Phaser.Game(config);
 
+let player;
 let ball;
-let leftPaddle;
-let rightPaddle;
-let cursors;
-let scoreLeft = 0;
-let scoreRight = 0;
-let scoreText;
+let aiPlayer;
+let playerScore = 0;
+let aiScore = 0;
+let ballSpeed = 200;
+let ballAngle = 0;
 
 function preload() {
-  this.load.image('ball', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/physics/ball.png');
+  this.load.image('player', 'https://uploads-ssl.webflow.com/59443eafc389bf3527eb8111/6411367dc858d930bab2fe22_paddle.svg');
+  this.load.image('ai', 'https://uploads-ssl.webflow.com/59443eafc389bf3527eb8111/6411367dc858d930bab2fe22_paddle.svg');
+  this.load.image('ball', 'https://uploads-ssl.webflow.com/59443eafc389bf3527eb8111/6411367d56f2916ff6c1468b_ball.svg');
 }
 
 function create() {
-  // Create ball
-  ball = this.physics.add.image(300, 200, 'ball');
+  // create the player
+  player = this.physics.add.sprite(30, 300, 'player');
+  player.setCollideWorldBounds(true);
+  player.setImmovable(true);
+
+  // create the AI player
+  aiPlayer = this.physics.add.sprite(770, 300, 'ai');
+  aiPlayer.setCollideWorldBounds(true);
+  aiPlayer.setImmovable(true);
+
+  // create the ball
+  ball = this.physics.add.sprite(400, 300, 'ball');
   ball.setCollideWorldBounds(true);
   ball.setBounce(1);
 
-  // Create left paddle with red fill
-  leftPaddle = this.physics.add.rectangle(50, 200, 10, 80, 0xFF0000);
-  leftPaddle.setImmovable(true);
+  // add collisions
+  this.physics.add.collider(player, ball, hitPlayer, null, this);
+  this.physics.add.collider(aiPlayer, ball, hitAiPlayer, null, this);
 
-  // Create right paddle with blue fill
-  rightPaddle = this.physics.add.rectangle(550, 200, 10, 80, 0x0000FF);
-  rightPaddle.setImmovable(true);
+  // add text for the score
+  const textStyle = { fontSize: '32px', fill: '#000' };
+  this.add.text(16, 16, 'Player: 0', textStyle);
+  this.add.text(650, 16, 'AI: 0', textStyle);
 
-  // Create score text
-  scoreText = this.add.text(300, 50, `${scoreLeft} - ${scoreRight}`, { fontFamily: 'Arial', fontSize: 32, color: '#FFFFFF' }).setOrigin(0.5);
+  // add keyboard input
+  this.input.keyboard.on('keydown-RETURN', startGame, this);
+  this.input.keyboard.on('keydown-UP', movePlayerUp, this);
+  this.input.keyboard.on('keydown-DOWN', movePlayerDown, this);
 
-  // Add keyboard input
-  cursors = this.input.keyboard.createCursorKeys();
-
-  // Start game on Enter key press
-  this.input.keyboard.on('keydown-ENTER', function() {
-    ball.setVelocityX(150);
-    ball.setVelocityY(-150);
-  });
+  // set the ball in motion
+  ball.setVelocity(ballSpeed, ballAngle);
 }
 
 function update() {
-  // Move left paddle with up and down arrow keys
-  if (cursors.up.isDown) {
-    leftPaddle.setVelocityY(-300);
-  } else if (cursors.down.isDown) {
-    leftPaddle.setVelocityY(300);
+  // move the AI player towards the ball
+  if (ball.y < aiPlayer.y) {
+    aiPlayer.setVelocityY(-200);
+  } else if (ball.y > aiPlayer.y) {
+    aiPlayer.setVelocityY(200);
   } else {
-    leftPaddle.setVelocityY(0);
+    aiPlayer.setVelocityY(0);
   }
 
-  // Prevent left paddle from leaving the screen
-  if (leftPaddle.y < 40) {
-    leftPaddle.y = 40;
-  } else if (leftPaddle.y > 360) {
-    leftPaddle.y = 360;
+  // check for scoring
+  if (ball.x < 0) {
+    aiScore++;
+    resetGame();
+  } else if (ball.x > 800) {
+    playerScore++;
+    resetGame();
   }
+}
 
-  // Move right paddle with W and S keys
-  if (cursors.W.isDown) {
-    rightPaddle.setVelocityY(-300);
-  } else if (cursors.S.isDown) {
-    rightPaddle.setVelocityY(300);
-  } else {
-    rightPaddle.setVelocityY(0);
-  }
+function startGame() {
+  ballSpeed = Phaser.Math.Between(200, 400);
+ballAngle = Phaser.Math.Between(-90, 90);
+ball.setVelocity(ballSpeed, ballAngle);
+}
 
-  // Prevent right paddle from leaving the screen
-  if (rightPaddle.y < 40) {
-    rightPaddle.y = 40;
-  } else if (rightPaddle.y > 360) {
-    rightPaddle.y = 360;
-  }
+function movePlayerUp() {
+player.setVelocityY(-300);
+}
 
-  // Update score text
-  scoreText.setText(`${scoreLeft} - ${scoreRight}`);
+function movePlayerDown() {
+player.setVelocityY(300);
+}
 
-  // Check for ball collision with paddles
-  this.physics.add.collider(ball, leftPaddle, function() {
-    ball.setVelocityX(300);
-    let diff = ball.y - leftPaddle.y;
+function hitPlayer() {
+ball.setVelocityY(Phaser.Math.Between(-120, 120));
+playerScore++;
+updateScore();
+}
+
+function hitAiPlayer() {
+ball.setVelocityY(Phaser.Math.Between(-120, 120));
+aiScore++;
+updateScore();
+}
+
+function resetGame() {
+ball.setVelocity(0);
+ball.setPosition(400, 300);
+player.setPosition(30, 300);
+aiPlayer.setPosition(770, 300);
+updateScore();
+}
+
+function updateScore() {
+const playerText = Player: ${playerScore};
+const aiText = AI: ${aiScore};
+const textStyle = { fontSize: '32px', fill: '#000' };
+this.children.list[0].setText(playerText);
+this.children.list[1].setText(aiText);
+}
