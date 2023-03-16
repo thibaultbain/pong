fca7922321e6e4d968758bd8efa0e099395f324a
thibaultbain <45803361+thibaultbain@users.noreply.github.com>
2023-03-16 21:25:53 +1100

    Update pong.js

--- pong.js
+++ pong.js
@@ -1,63 +1,73 @@
+		// Define game config object
 		var config = {
 			type: Phaser.AUTO,
 			scale: {
-				mode: Phaser.Scale.FIT,
 				parent: 'game-container',
-				autoCenter: Phaser.Scale.CENTER_BOTH
+				mode: Phaser.Scale.FIT,
+				autoCenter: Phaser.Scale.CENTER_BOTH,
+				width: 800,
+				height: 600
 			},
-			width: 800,
-			height: 600,
-			backgroundColor: '#000000',
 			scene: {
 				preload: preload,
 				create: create,
 				update: update
-			}
+			},
+			backgroundColor: '#000000'
 		};
 
+		// Initialize game with config object
 		var game = new Phaser.Game(config);
 
-		var userPaddle;
-		var aiPaddle;
 		var ball;
-		var ballVelocity = 200;
+		var leftPaddle;
+		var rightPaddle;
+		var upKey;
+		var downKey;
 
 		function preload() {
-			this.load.image('paddle', 'https://uploads-ssl.webflow.com/59443eafc389bf3527eb8111/6411367dc858d930bab2fe22_paddle.svg');
-			this.load.image('ball', 'https://uploads-ssl.webflow.com/59443eafc389bf3527eb8111/6411367d56f2916ff6c1468b_ball.svg');
+			// Load assets
+			this.load.svg('paddle', 'https://uploads-ssl.webflow.com/59443eafc389bf3527eb8111/6411367dc858d930bab2fe22_paddle.svg', {
+				width: 25,
+				height: 100
+			});
+			this.load.svg('ball', 'https://uploads-ssl.webflow.com/59443eafc389bf3527eb8111/6411367d56f2916ff6c1468b_ball.svg', {
+				width: 20,
+				height: 20
+			});
 		}
 
 		function create() {
-			userPaddle = this.physics.add.sprite(50, this.cameras.main.centerY, 'paddle').setScale(0.5);
-			userPaddle.setCollideWorldBounds(true);
-			userPaddle.setImmovable(true);
+			// Create sprites
+			ball = this.add.sprite(game.config.width / 2, game.config.height / 2, 'ball');
+			leftPaddle = this.add.sprite(50, game.config.height / 2, 'paddle');
+			rightPaddle = this.add.sprite(game.config.width - 50, game.config.height / 2, 'paddle');
 
-			aiPaddle = this.physics.add.sprite(this.cameras.main.width - 50, this.cameras.main.centerY, 'paddle').setScale(0.5);
-			aiPaddle.setCollideWorldBounds(true);
-			aiPaddle.setImmovable(true);
+			// Add physics to sprites
+			this.physics.add.existing(ball);
+			this.physics.add.existing(leftPaddle);
+			this.physics.add.existing(rightPaddle);
 
-			ball = this.physics.add.sprite(this.cameras.main.centerX, this.cameras.main.centerY, 'ball').setScale(0.5);
-			ball.setCollideWorldBounds(true);
-			ball.setBounce(1, 1);
-			ball.setVelocity(ballVelocity, ballVelocity);
+			// Make paddles immovable
+			leftPaddle.body.immovable = true;
+			rightPaddle.body.immovable = true;
+
+			// Add collision detection between ball and paddles
+			this.physics.add.collider(ball, leftPaddle);
+			this.physics.add.collider(ball, rightPaddle);
+
+			// Set up keyboard controls
+			upKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+			downKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
 		}
 
 		function update() {
-			this.input.keyboard.on('keydown-RETURN', function () {
-				ball.setVelocity(ballVelocity, ballVelocity);
-			});
-
-			userPaddle.setVelocityY(0);
-
-			if (this.input.keyboard.checkDown(this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP), 500)) {
-				userPaddle.setVelocityY(-400);
-			} else if (this.input.keyboard.checkDown(this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN), 500)) {
-				userPaddle.setVelocityY(400);
-			}
-
-			if (ball.y < aiPaddle.y) {
-				aiPaddle.setVelocityY(-400);
-			} else if (ball.y > aiPaddle.y) {
-				aiPaddle.setVelocityY(400);
+			// Move left paddle up or down based on arrow key input
+			if (upKey.isDown) {
+				leftPaddle.body.setVelocityY(-300);
+			} else if (downKey.isDown) {
+				leftPaddle.body.setVelocityY(300);
+			} else {
+				leftPaddle.body.setVelocityY(0);
 			}
 		}
