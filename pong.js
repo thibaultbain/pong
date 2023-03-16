27f3d837198ba13cc36d8b449622752fbac11d4a
thibaultbain <45803361+thibaultbain@users.noreply.github.com>
2023-03-16 21:57:58 +1100

    Update pong.js

--- pong.js
+++ pong.js
@@ -1,56 +1,68 @@
-      var config = {
-        type: Phaser.AUTO,
-        width: window.innerWidth,
-        height: window.innerHeight,
-        backgroundColor: '#000000',
-        physics: {
-          default: 'arcade',
-          arcade: {
-            gravity: { y: 0 },
-            debug: false
-          }
-        },
-        scene: {
-          preload: preload,
-          create: create,
-          update: update
-        }
-      };
-      
-      var game = new Phaser.Game(config);
-      var ball;
-      var paddle;
-      var cursors;
-      
-      function preload () {
-        this.load.image('ball', 'https://uploads-ssl.webflow.com/59443eafc389bf3527eb8111/6411367d56f2916ff6c1468b_ball.svg');
-        this.load.image('paddle', 'https://uploads-ssl.webflow.com/59443eafc389bf3527eb8111/6411367dc858d930bab2fe22_paddle.svg');
-      }
-      
-      function create () {
-        ball = this.physics.add.image(config.width/2, config.height/2, 'ball').setScale(0.03);
-        paddle = this.physics.add.image(30, config.height/2, 'paddle').setScale(0.1);
-        paddle.setCollideWorldBounds(true);
-        paddle.setImmovable(true);
-        cursors = this.input.keyboard.createCursorKeys();
-        this.input.keyboard.on('keydown', function (event) {
-          if (event.code === 'Enter') {
-            ball.setVelocityX(200);
-            ball.setVelocityY(200);
-          }
-        });
-      }
-      
-      function update () {
-        if (cursors.up.isDown) {
-          paddle.setVelocityY(-200);
-        } else if (cursors.down.isDown) {
-          paddle.setVelocityY(200);
-        } else {
-          paddle.setVelocityY(0);
-        }
-        
-        this.physics.add.collider(ball, paddle, function () {
-          ball.setVelocityX(400);
-        }, null, this);
-      }
+const config = {
+  type: Phaser.AUTO,
+  parent: 'game-container',
+  backgroundColor: 0x000000,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: 800,
+    height: 600,
+  },
+  scene: {
+    preload: preload,
+    create: create,
+    update: update,
+  },
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: false,
+    },
+  },
+};
+
+const game = new Phaser.Game(config);
+let ball, leftPaddle, rightPaddle, cursors;
+
+function preload() {
+  this.load.image('ball', 'https://uploads-ssl.webflow.com/59443eafc389bf3527eb8111/6411367d56f2916ff6c1468b_ball.svg');
+  this.load.image('paddle', 'https://uploads-ssl.webflow.com/59443eafc389bf3527eb8111/6411367dc858d930bab2fe22_paddle.svg');
+}
+
+function create() {
+  leftPaddle = this.physics.add.image(0, config.height / 2, 'paddle').setOrigin(0, 0.5).setImmovable();
+  rightPaddle = this.physics.add.image(config.width, config.height / 2, 'paddle').setOrigin(1, 0.5).setImmovable();
+  ball = this.physics.add.image(config.width / 2, config.height / 2, 'ball').setOrigin(0.5);
+  ball.setCollideWorldBounds(true);
+  ball.setBounce(1);
+  this.physics.add.collider(ball, leftPaddle, paddleBounce, null, this);
+  this.physics.add.collider(ball, rightPaddle, paddleBounce, null, this);
+
+  cursors = this.input.keyboard.createCursorKeys();
+  this.input.keyboard.on('keydown-ENTER', startGame, this);
+}
+
+function update() {
+  if (ball.body.velocity.x === 0 && ball.body.velocity.y === 0) return;
+
+  if (cursors.up.isDown) {
+    leftPaddle.setVelocityY(-300);
+  } else if (cursors.down.isDown) {
+    leftPaddle.setVelocityY(300);
+  } else {
+    leftPaddle.setVelocityY(0);
+  }
+
+  rightPaddle.y = ball.y;
+}
+
+function startGame() {
+  if (ball.body.velocity.x === 0 && ball.body.velocity.y === 0) {
+    ball.setVelocity(Phaser.Math.Between(-200, 200), Phaser.Math.Between(-200, 200));
+  }
+}
+
+function paddleBounce(ball, paddle) {
+  ball.setVelocityX(ball.body.velocity.x * -1.1);
+  ball.setVelocityY(Phaser.Math.Between(-50, 50));
+}
