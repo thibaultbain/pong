1ba973fc1486b6d4c6dfb791dfee9debe105fe9a
thibaultbain <45803361+thibaultbain@users.noreply.github.com>
2023-03-16 21:30:50 +1100

    Update pong.js

--- pong.js
+++ pong.js
@@ -1,73 +1,70 @@
-		// Define game config object
-		var config = {
-			type: Phaser.AUTO,
-			scale: {
-				parent: 'game-container',
-				mode: Phaser.Scale.FIT,
-				autoCenter: Phaser.Scale.CENTER_BOTH,
-				width: 800,
-				height: 600
-			},
-			scene: {
-				preload: preload,
-				create: create,
-				update: update
-			},
-			backgroundColor: '#000000'
-		};
+      const config = {
+        type: Phaser.AUTO,
+        width: window.innerWidth,
+        height: window.innerHeight,
+        parent: 'game-container',
+        backgroundColor: '#000000',
+        scene: {
+          preload: preload,
+          create: create,
+          update: update
+        }
+      };
 
-		// Initialize game with config object
-		var game = new Phaser.Game(config);
+      const game = new Phaser.Game(config);
 
-		var ball;
-		var leftPaddle;
-		var rightPaddle;
-		var upKey;
-		var downKey;
+      let ball;
+      let paddle;
+      let aiPaddle;
 
-		function preload() {
-			// Load assets
-			this.load.svg('paddle', 'https://uploads-ssl.webflow.com/59443eafc389bf3527eb8111/6411367dc858d930bab2fe22_paddle.svg', {
-				width: 25,
-				height: 100
-			});
-			this.load.svg('ball', 'https://uploads-ssl.webflow.com/59443eafc389bf3527eb8111/6411367d56f2916ff6c1468b_ball.svg', {
-				width: 20,
-				height: 20
-			});
-		}
+      function preload() {
+        this.load.image('ball', 'https://uploads-ssl.webflow.com/59443eafc389bf3527eb8111/6411367d56f2916ff6c1468b_ball.svg');
+        this.load.image('paddle', 'https://uploads-ssl.webflow.com/59443eafc389bf3527eb8111/6411367dc858d930bab2fe22_paddle.svg');
+      }
 
-		function create() {
-			// Create sprites
-			ball = this.add.sprite(game.config.width / 2, game.config.height / 2, 'ball');
-			leftPaddle = this.add.sprite(50, game.config.height / 2, 'paddle');
-			rightPaddle = this.add.sprite(game.config.width - 50, game.config.height / 2, 'paddle');
+      function create() {
+        ball = this.physics.add.sprite(config.width / 2, config.height / 2, 'ball');
+        ball.setCollideWorldBounds(true);
+        ball.setBounce(1);
 
-			// Add physics to sprites
-			this.physics.add.existing(ball);
-			this.physics.add.existing(leftPaddle);
-			this.physics.add.existing(rightPaddle);
+        paddle = this.physics.add.sprite(20, config.height / 2, 'paddle');
+        paddle.setCollideWorldBounds(true);
+        paddle.setImmovable(true);
 
-			// Make paddles immovable
-			leftPaddle.body.immovable = true;
-			rightPaddle.body.immovable = true;
+        aiPaddle = this.physics.add.sprite(config.width - 20, config.height / 2, 'paddle');
+        aiPaddle.setCollideWorldBounds(true);
+        aiPaddle.setImmovable(true);
 
-			// Add collision detection between ball and paddles
-			this.physics.add.collider(ball, leftPaddle);
-			this.physics.add.collider(ball, rightPaddle);
+        this.physics.add.collider(ball, paddle, function () {
+          ball.setVelocityY(-1 * (Math.random() * 400 + 200));
+        });
 
-			// Set up keyboard controls
-			upKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
-			downKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
-		}
+        this.physics.add.collider(ball, aiPaddle, function () {
+          ball.setVelocityY(-1 * (Math.random() * 400 + 200));
+        });
 
-		function update() {
-			// Move left paddle up or down based on arrow key input
-			if (upKey.isDown) {
-				leftPaddle.body.setVelocityY(-300);
-			} else if (downKey.isDown) {
-				leftPaddle.body.setVelocityY(300);
-			} else {
-				leftPaddle.body.setVelocityY(0);
-			}
-		}
+        this.input.keyboard.on('keydown-ENTER', function () {
+          ball.setVelocityX(-150);
+          ball.setVelocityY(-150);
+        });
+
+        this.input.keyboard.on('keydown-UP', function () {
+          paddle.setVelocityY(-300);
+        });
+
+        this.input.keyboard.on('keydown-DOWN', function () {
+          paddle.setVelocityY(300);
+        });
+
+        this.input.keyboard.on('keyup-UP', function () {
+          paddle.setVelocityY(0);
+        });
+
+        this.input.keyboard.on('keyup-DOWN', function () {
+          paddle.setVelocityY(0);
+        });
+      }
+
+      function update() {
+        aiPaddle.y = ball.y;
+      }
