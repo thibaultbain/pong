26ad064e3df60eb8e50f882bb463c7e57707e62e
thibaultbain <45803361+thibaultbain@users.noreply.github.com>
2023-03-17 15:46:51 +1100

    Update pong.js

--- pong.js
+++ pong.js
@@ -1,13 +1,12 @@
-const gameContainer = document.getElementById('game-container');
 const config = {
   type: Phaser.AUTO,
   parent: 'game-container',
   backgroundColor: 0x000000,
   scale: {
-    mode: Phaser.Scale.RESIZE,
+    mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
-    width: gameContainer.clientWidth,
-    height: gameContainer.clientHeight,
+    width: 800,
+    height: 600,
   },
   scene: {
     create: create,
@@ -16,111 +15,86 @@ const config = {
 };
 
 const game = new Phaser.Game(config);
-let ball, leftPaddle, rightPaddle;
-let cursors;
-let gameStarted = false;
-let aiSpeed = 250;
+let ball, leftPaddle, rightPaddle, cursors, gameStarted;
 
 function create() {
-  leftPaddle = this.add.rectangle(0, this.scale.height / 2, 20, 100, 0xffffff).setOrigin(0, 0.5);
+  leftPaddle = this.add.rectangle(0, this.scale.height / 2, 20, 100, 0xffffff);
   rightPaddle = this.add.rectangle(this.scale.width, this.scale.height / 2, 20, 100, 0xffffff).setOrigin(1, 0.5);
-
   ball = this.add.circle(this.scale.width / 2, this.scale.height / 2, 10, 0xffffff);
-  this.physics.add.existing(ball);
-  ball.body.setCollideWorldBounds(true, 1, 1);
-  ball.body.setBounce(1, 1);
-
-  this.physics.add.existing(leftPaddle, true);
-  this.physics.add.existing(rightPaddle, true);
+  ball.setOrigin(0.5);
 
   cursors = this.input.keyboard.createCursorKeys();
   this.input.keyboard.on('keydown-ENTER', startGame, this);
+  gameStarted = false;
 
   this.score = 0;
-  updateScoreDisplay.call(this);
-
-  setupTouchControls.call(this);
+  setupTouchControls();
 }
 
 function update() {
-  if (gameStarted) {
-    ball.body.setVelocityX(300);
-    ball.body.setVelocityY(200);
+  if (!gameStarted) return;
 
-    this.physics.world.collide(ball, leftPaddle, paddleCollide);
-    this.physics.world.collide(ball, rightPaddle, paddleCollide);
+  ball.x += ball.vx * this.game.loop.delta / 1000;
+  ball.y += ball.vy * this.game.loop.delta / 1000;
 
-    if (cursors.up.isDown) {
-      leftPaddle.y -= 10;
-    } else if (cursors.down.isDown) {
-      leftPaddle.y += 10;
-    }
+  if (ball.y < 0 || ball.y > this.scale.height) {
+    ball.vy *= -1;
+  }
 
-    leftPaddle.y = Phaser.Math.Clamp(leftPaddle.y, leftPaddle.height / 2, this.scale.height - leftPaddle.height / 2);
+  if (Phaser.Geom.Intersects.RectangleToRectangle(ball.getBounds(), leftPaddle.getBounds()) || Phaser.Geom.Intersects.RectangleToRectangle(ball.getBounds(), rightPaddle.getBounds())) {
+    ball.vx *= -1.1;
+  }
 
-    if (ball.y < rightPaddle.y) {
-      rightPaddle.y -= aiSpeed * this.time.physicsElapsed;
-    } else {
-      rightPaddle.y += aiSpeed * this.time.physicsElapsed;
-    }
-  } else {
-    ball.body.setVelocity(0, 0);
-    ball.x = this.scale.width / 2;
-    ball.y = this.scale.height / 2;
+  if (Phaser.Geom.Intersects.RectangleToRectangle(ball.getBounds(), rightPaddle.getBounds())) {
+    this.score++;
+    updateScoreDisplay.call(this);
   }
 
   if (ball.x < 0 || ball.x > this.scale.width) {
     ball.x = this.scale.width / 2;
     ball.y = this.scale.height / 2;
     gameStarted = false;
-    this.score = 0;
-    updateScoreDisplay.call(this);
   }
 
-  if (ball.body.touching.right) {
-    this.score++;
-    updateScoreDisplay.call(this);
+  if (cursors.up.isDown) {
+    leftPaddle.y -= 300 * this.game.loop.delta / 1000;
+  } else if (cursors.down.isDown) {
+    leftPaddle.y += 300 * this.game.loop.delta / 1000;
   }
-}
 
-function paddleCollide(ball, paddle) {
-  ball.setVelocityY(ball.body.velocity.y + (ball.y - paddle.y) / 2);
+  leftPaddle.y = Phaser.Math.Clamp(leftPaddle.y, leftPaddle.height / 2, this.scale.height - leftPaddle.height / 2);
+
+  rightPaddle.y = ball.y;
 }
 
 function startGame() {
   if (!gameStarted) {
+    ball.vx = Phaser.Math.Between(-300, 300);
+    ball.vy = Phaser.Math.Between(-300, 300);
     gameStarted = true;
-    ball.body.setVelocityX(-300);
-    ball.body.setVelocityY(200);
+    this.score = 0;
+    updateScoreDisplay.call(this);
   }
 }
 
-function setupTouchControls() {
-  const startButton = document.getElementById('start-button');
-  const upButton = document.getElementById('up-button');
-const downButton = document.getElementById('down-button');
-
-startButton.addEventListener('mousedown', startGame.bind(this));
-startButton.addEventListener('touchstart', startGame.bind(this));
-
-upButton.addEventListener('mousedown', () => cursors.up.isDown = true);
-upButton.addEventListener('mouseup', () => cursors.up.isDown = false);
-upButton.addEventListener('touchstart', (e) => {
-e.preventDefault();
-cursors.up.isDown = true;
-});
-upButton.addEventListener('touchend', () => cursors.up.isDown = false);
-
-downButton.addEventListener('mousedown', () => cursors.down.isDown = true);
-downButton.addEventListener('mouseup', () => cursors.down.isDown = false);
-downButton.addEventListener('touchstart', (e) => {
-e.preventDefault();
-cursors.down.isDown = true;
-});
-downButton.addEventListener('touchend', () => cursors.down.isDown = false);
+function updateScoreDisplay() {
+  const scoreElement = document.getElementById('score');
+  scoreElement.textContent = this.score;
 }
 
-function updateScoreDisplay() {
-const scoreElement = document.getElementById('score');
-scoreElement.textContent = this.score;
+function setupTouchControls() {
+  const upButton = document.getElementById('up-button');
+  const downButton = document.getElementById('down-button');
+
+  upButton.addEventListener('mousedown', () => { cursors.up.isDown = true; });
+  upButton.addEventListener('touchstart', () => { cursors.up.isDown = true; });
+  upButton.addEventListener('mouseup', () => { cursors.up.isDown = false; });
+  upButton.addEventListener('mouseleave', () => { cursors.up.isDown = false; });
+  upButton.addEventListener('touchend', () => { cursors.up.isDown = false; });
+
+downButton.addEventListener('mousedown', () => { cursors.down.isDown = true; });
+downButton.addEventListener('touchstart', () => { cursors.down.isDown = true; });
+downButton.addEventListener('mouseup', () => { cursors.down.isDown = false; });
+downButton.addEventListener('mouseleave', () => { cursors.down.isDown = false; });
+downButton.addEventListener('touchend', () => { cursors.down.isDown = false; });
 }
